947b12ed0598751e0d8ba8cff82e633f6de7db30
Stephen Simpson <me@simpo.org>
2011-01-08 14:36:25 +0000

    Added phpDocumentor comments

--- controllers/main.php
+++ controllers/main.php
@@ -1,14 +1,28 @@
 <?php
-//Include main modules
+/**
+ *	Main controller for the Impact platform.
+ *
+ *	@author Stephen Simpson <me@simpo.org>
+ *	@version 0.1
+ *	@license http://www.gnu.org/licenses/lgpl.html LGPL
+ *	
+ */
+
+//Include the core/base classes, needed throughout
 require_once(ROOT_BACK.'/models/superclass.php');
 require_once(ROOT_BACK.'/models/class.Impact.php');
 require_once(ROOT_BACK.'/models/class.Database.php');
 
+//Load the main application class and database class
 $impact = Impact::singleton();
 $impact->setup();
 $database = Database::singleton();
 
 
+/**
+ *	Check that the page/item being requested is valid,
+ *	then load page, pass through the template parser and pass to the selected view
+ */
 if ($impact->pageErrorCheck) {
 	$tparser = $impact->factory('templater');
 	$tparser->init($database->getPage(),$impact->application);
--- models/superclass.php
+++ models/superclass.php
@@ -1,23 +1,48 @@
 <?php
 /**
-*		Main database class
+*	Main base class.
+*
+*	Class is used as a base for most of the classes throughout the platform.
+*	It includes all the functions that are necessary to work
+*	using the, "Impact formula".
 *		
-*		@Author: Stephen Simpson
-*		@Version: 0.0.1
-*		@License: LGPL
+*	@author Stephen Simpson <me@simpo.org>
+*	@version 0.1
+*	@license http://www.gnu.org/licenses/lgpl.html LGPL
+*	@package Impact
 *		
 */
 class Impact_Superclass {
+	
+	/**
+	 *	Factory method
+	 *
+	 *	The factory method is used to generate new classes according to the
+	 *	standard rules of the Impact Platform.  The class is sometimes overidden
+	 *	in base-classes if the class has it's own sub-classes that are loaded
+	 *	on-the-fly.  Classes are only included if they are used,
+	 *	saving load over-heads.  The factory method knows where to find the files.
+	 *
+	 *	@access public
+	 *	@static
+	 *	@param String $className The name of the class to load.
+	 *	@todo Method needs to be made more generic so it isn't overidden in subclasses
+	*/
 	public static function factory($className) {
 		$dir = self::get_include_directory();
 		
-        if (include_once '/class.'.str_replace('_','.',$className).'.php') {
+		if (include_once '/class.'.str_replace('_','.',$className).'.php') {
 			return new $className;
-        } else {
-            throw new Exception($className.' Class not found');
-        }
-    }
+		} else {
+			throw new Exception($className.' Class not found');
+		}
+	}
 	
+	/**
+	*	Get the the file location of the current class.
+	*
+	*	@access public
+	*/
 	public function get_include_directory() {
 		$debug = debug_backtrace();
 		return dirname($debug[0][file]);
