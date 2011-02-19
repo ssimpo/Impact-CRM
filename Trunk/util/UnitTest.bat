88ee86167380e9c84babbf3bdabff4e60480c561
ssimpo <ssimpo@7b11f61c-ac1f-418c-b1a8-e4117cb018a3>
2011-02-19 14:35:21 +0000

    * Small modification to the bat files so the echoed messages are better * Changes to The Komodo Project file so single files can be code-checked and added some hooks into TortoiseSVN

--- Trunk/util/UnitTest.bat
+++ Trunk/util/UnitTest.bat
@@ -1,15 +1,15 @@
 @echo off
-echo Running Unit Tests on Impact Project...
-
 if defined "%2" goto :special else goto :all
 
 :special
 if "%2" == "all" goto :all
 if "%2" == "" goto :all
+echo Running Unit Tests contained in: unit_tests\%2...
 call phpunit unit_tests\%2 > unittest.log
 goto :end
 
 :all
+echo Running Unit Tests on Impact Project...
 call phpunit unit_tests\ > unittest.log
 goto :end
 
