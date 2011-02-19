d1be3fbf9f4dd46fdc2f4ad01c2bdee86a7b5953
Stephen Simpson <me@simpo.org>
2011-02-19 14:35:21 +0000

    * Small modification to the bat files so the echoed messages are better * Changes to The Komodo Project file so single files can be code-checked and added some hooks into TortoiseSVN

--- util/CodeCheck.bat
+++ util/CodeCheck.bat
@@ -1,6 +1,4 @@
 @echo off
-echo Running Code Checks on Impact Project...
-
 if defined "%2" goto :special else goto :all
 
 :special
@@ -8,21 +6,29 @@ if "%2" == "all" goto :all
 if "%2" == "" goto :all
 if "%2" == "impact" goto :impact
 if "%2" == "pear" goto :pear
-goto :all
+
+:onefile
+echo Running Code Checks on: %2...
+call phpcs -s --standard=Impact %2 > codecheck_impact.log
+%1 codecheck_impact.log
+goto :end
 
 :impact
+echo Running Impact Code Checks on Impact Project...
 call phpcs --standard=Impact ../models/ ../controllers/ ../plugins/ ../index.php unit_tests/models/ unit_tests/plugins/ unit_tests/controllers/ > codecheck_impact.log
 %1 codecheck_impact.log
 goto :end
 
 :pear
-call phpcs --standard=PEAR PEAR/ > codecheck_pear.log
+echo Running PEAR Code Checks on Impact Project...
+call phpcs -s --standard=PEAR PEAR/ > codecheck_pear.log
 %1 codecheck_pear.log
 goto :end
 
 :all
-call phpcs --standard=Impact ../models/ ../controllers/ ../plugins/ ../index.php unit_tests/models/ unit_tests/plugins/ unit_tests/controllers/ > codecheck_impact.log
-call phpcs --standard=PEAR PEAR/ > codecheck_pear.log
+echo Running Code Checks on Impact Project...
+call phpcs -s --standard=Impact ../models/ ../controllers/ ../plugins/ ../index.php unit_tests/models/ unit_tests/plugins/ unit_tests/controllers/ > codecheck_impact.log
+call phpcs -s --standard=PEAR PEAR/ > codecheck_pear.log
 %1 codecheck_impact.log
 %1 codecheck_pear.log
 goto :end
