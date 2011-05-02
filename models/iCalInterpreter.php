007f4e67019ad7dec89662bd728fe3a624e854f7
Stephen Simpson <me@simpo.org>
2011-05-02 14:23:47 +0000

    This module has been completely re-worked.  It will create an array tree from iCal data (via the Savi class).  Currently only supports main blocks - no parsing of block content yet.

--- models/iCalInterpreter.php
+++ models/iCalInterpreter.php
@@ -3,74 +3,172 @@
  *	iCal Interpreter, requires the SAVI parser for iCal/vCalendar/vCard
  *		
  *	@author Stephen Simpson <me@simpo.org>
- *	@version 0.0.3
+ *	@version 0.0.4
  *	@license http://www.gnu.org/licenses/lgpl.html LGPL
  *	@package Calendar	
  */
-class Ical_Interpreter {
-	private $SAVI = '';
-	private $data = '';
+class iCalInterpreter {
+	private $savi = '';
+	private $data = array();
 	private $block = '';
 	private $subblock = '';
+	private $treePos;
 	
 	public function __construct() {
-		$this->SAVI = new Savi_Parser();
-		$this->SAVI->ical_set_element_handler(
-			$this->SAVI,
+		$this->savi = new Savi();
+		
+		$this->savi->ical_set_element_handler(
+			$this->savi,
 			array($this,'start_tag'),array($this,'end_tag')
 		);
-		$this->SAVI->ical_set_character_data_handler(
-			$this->SAVI,
+		$this->savi->ical_set_character_data_handler(
+			$this->savi,
 			array($this,'text_content')
 		);
-		$this->treePos = $data;
+		$this->treePos =& $this->data;
 	}
 	
 	public function parse($filename) {
-		$this->data = array(
-			'VEVENT'=>array(), 'VJOURNAL'=>array(), 'VTODO'=>array(), 
-			'VTIMEZONE'=>array(), 'VFREEBUSY'=>array()
-		);
-		$this->SAVI->ical_parse($this->SAVI,$filename);
-		echo '<pre>';print_r($this->data);echo '</pre>';
+		$this->savi->ical_parse($this->savi,$filename);
+		return $this->data;
 	}
 	
 	public function start_tag($parser,$name,$attributes,$content='') {
-		switch ($name) {
-			case 'VCALENDAR':
-				$this->block = ''; $this->subblock = '';
-				break;
-			case 'VEVENT': case 'VJOURNAL': case 'VTODO' : case 'VTIMEZONE': case 'VFREEBUSY':
-				$this->block = $name; $this->subblock = '';
-				break;
-			case 'VALRAM': case 'DAYLIGHT':
-				$this->subblock = $name;
-				$this->data[$this->block][$name] = array(0=>$carray);
-				break;
-			default:
-				$carray = array('CONTENT'=>$content, 'ATTRIBUTES'=>$attributes);
-				if (($this->subblock == '') && ($this->block == '')) {
-					//$this->data[$name] = $carray;
-					if (array_key_exists($name,$this->data)) {
-						array_push($this->data[$name],$carray);
-					} else {
-						$this->data[$name] = array(0=>$carray);
-					}
-				} elseif ($this->subblock == '') {
-					if (array_key_exists($name,$this->data[$this->block])) {
-						array_push($this->data[$this->block][$name],$carray);
-					} else {
-						$this->data[$this->block][$name] = array(0=>$carray);
-					}
-				} else {
-					if (array_key_exists($name,$this->data[$this->block][$this->subblock])) {
-						array_push($this->data[$this->block][$this->subblock][$name],$carray);
-					} else {
-						$this->data[$this->block][$this->subblock][$name] = array(0=>$carray);
-					}
-				}
+		$carray = array('CONTENT'=>$content, 'ATTRIBUTES'=>$attributes);
+		
+		$handler = array($this,'_handle_'.strtolower($name));
+		if (is_callable($handler)) {
+			call_user_func($handler,$this,$name,$attributes,$content);
+		} else {
+			
 		}
-		//echo '<p><b>TAG:</b> '.$name.'<br />'.$content.'</p>';
+	}
+	
+	/**
+	 *	Handle a VCALENDAR tag.
+	 *
+	 *	@private
+	 */
+	private function _handle_vcalendar() {
+		$this->block = '';
+		$this->subblock = '';
+		$this->treePos =& $this->data;
+	}
+	
+	/**
+	 *	Handle a VEVENT tag.
+	 *
+	 *	@private
+	 */
+	private function _handle_vevent() {
+		$this->_new_block('VEVENT');
+	}
+	
+	/**
+	 *	Handle a VTODO tag.
+	 *
+	 *	@private
+	 */
+	private function _handle_vtodo() {
+		$this->_new_block('VTODO');
+	}
+	
+	/**
+	 *	Handle a VJOURNAL tag.
+	 *
+	 *	@private
+	 */
+	private function _handle_vjournal() {
+		$this->_new_block('VJOURNAL');
+	}
+	
+	/**
+	 *	Handle a VTIMEZONE tag.
+	 *
+	 *	@private
+	 */
+	private function _handle_vtimezone() {
+		$this->_new_block('VTIMEZONE');
+	}
+	
+	/**
+	 *	Handle a VFREEBUSY tag.
+	 *
+	 *	@private
+	 */
+	private function _handle_vfreebusy() {
+		$this->_new_block('VFREEBUSY');
+	}
+	
+	/**
+	 *	Handle a VALARM tag.
+	 *
+	 *	@private
+	 */
+	private function _handle_valarm() {
+		$this->_new_subblock('VALARM');
+	}
+	
+	/**
+	 *	Handle a DAYLIGHT tag.
+	 *
+	 *	@private
+	 */
+	private function _handle_daylight() {
+		$this->_new_subblock('DAYLIGHT');
+	}
+	
+	/**
+	 *	Handle a STANDARD tag.
+	 *
+	 *	@private
+	 */
+	private function _handle_standard() {
+		$this->_new_subblock('STANDARD');
+	}
+	
+	/**
+	 *	Generate a new block element in the current data tree.
+	 *
+	 *	@private
+	 *	@param string $name The name of the block element to create.
+	 */
+	private function _new_block($name) {
+		$this->block = $name;
+		$this->subblock = '';
+		$this->treePos =& $this->data;
+		
+		$this->_new_block_generic($name);
+	}
+	
+	/**
+	 *	Generate a new subblock element at current tree position.
+	 *
+	 *	@private
+	 *	@param string $name The name of the subblock element to create.
+	 */
+	private function _new_subblock($name) {
+		if ($this->subblock != '') { //Return to parent
+			$this->treePos =& $this->data[$this->block];
+			$this->treePos =& $this->treePos[count($this->treePos)-1];
+		}
+		$this->subblock = $name;
+		$this->_new_block_generic($name);
+	}
+	
+	/**
+	 *	Generate a new block element at current tree position.
+	 *
+	 *	@private
+	 *	@param string $name The name of the block element to create.
+	 */
+	private function _new_block_generic($name) {
+		if (!array_key_exists($name,$this->treePos)) {
+			$this->treePos[$name] = array();
+		}
+		$this->treePos =& $this->treePos[$name];
+		$end = array_push($this->treePos,array());
+		$this->treePos =& $this->treePos[$end-1];
 	}
 
 	public function end_tag($parser,$name) {
@@ -82,16 +180,7 @@ class Ical_Interpreter {
 	}
 	
 	private function _add_element_get_ref($key) {
-		if (array_key_exists($key,$this->treePos)) {
-			echo '<h1>1</h1>';
-			$this->treePos[$key][count($this->treePos[$key])] = array();
-			return $this->treePos[$key][count($this->treePos[$key])-1];
-		} else {
-			echo '<h1>2</h1>';
-			$this->treePos[$key] = array('PARENT'=>$this->treePos);
-			$this->treePos[$key][0] = array('PARENT'=>$this->treePos[$key]);
-			return $this->treePos[$key][0];
-		}
+		
 	}
 
 }
