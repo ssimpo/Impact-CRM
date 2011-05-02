6a1b8c96ab7f99bf1b85d1a9f9f32c8cc0671e6d
ssimpo <ssimpo@7b11f61c-ac1f-418c-b1a8-e4117cb018a3>
2011-05-02 21:14:42 +0000

    Added a timezone class.

---
+++ Trunk/models/Calendar/Timezone.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *	Calendar.Timezone class
+ *		
+ *	@author Stephen Simpson <me@simpo.org>
+ *	@version 0.0.3
+ *	@license http://www.gnu.org/licenses/lgpl.html LGPL
+ *	@package Calendar
+ *	@extends Calendar_Base
+ */
+class Calendar_Timezone Extends CalendarBase implements Calendar_Object {
+
+    public function __construct() {
+	parent::__construct();
+    }
+}
