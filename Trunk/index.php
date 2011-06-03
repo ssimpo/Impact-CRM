b76a7e9e864bc7cfa4b4b9fcc84c8a15c405160f
ssimpo <ssimpo@7b11f61c-ac1f-418c-b1a8-e4117cb018a3>
2011-06-03 10:51:35 +0000

    Move the global setup to its own file.  This may be a temporary solution?

--- Trunk/index.php
+++ Trunk/index.php
@@ -1,41 +1,4 @@
 <?php
-if (!defined('__DIR__')) { 
-    $iPos = strrpos(__FILE__, "/"); 
-    define("__DIR__", substr(__FILE__, 0, $iPos) . "/"); 
-}
-
-define('DS', DIRECTORY_SEPARATOR);
-define('ROOT_BACK', __DIR__.DS);
-define('DOMAIN', $_SERVER['HTTP_HOST']);
-
-load_config(ROOT_BACK.'config'.DS.'settings.xml');
+require_once('globals.php');
 require_once ROOT_BACK.CONTROLLERS_DIRECTORY.DS.'main.php';
-
-
-/**
- * Load a config file
- *
- * 	Loads a config file (XML) and returns it's values as an array. String-values
- * 	are returned as strings and integer-values as integers.
- *
- *	@param String $path Location of the settings file.
- *	@return string()|integer()
- *	@todo Make it work with more complex data types.
-*/
-function load_config($path) {
-	$config = simplexml_load_file($path);
-        
-	foreach ($config->param as $param) {
-		if (!defined($param['name'])) {
-			switch ($param['type']) {
-				case 'string':
-					define($param['name'],$param['value']);
-					break;
-				case 'integer':
-					define($param['name'],(int) $param['value']);
-					break;
-			}
-		}
-	}
-}
 ?>
