422aee4ea7a6e472d135b794e8c88f0a76e1e544
Stephen Simpson <me@simpo.org>
2011-06-03 10:41:37 +0000

    Moving the config.xml loader to index.php is not ideal but it means that all the global constants are created from the start allowing more constancy.

--- index.php
+++ index.php
@@ -8,7 +8,34 @@ define('DS', DIRECTORY_SEPARATOR);
 define('ROOT_BACK', __DIR__.DS);
 define('DOMAIN', $_SERVER['HTTP_HOST']);
 
-require_once ROOT_BACK.'models'.DS.'I.php';
-I::load_config(ROOT_BACK.'config'.DS.'settings.xml');
+load_config(ROOT_BACK.'config'.DS.'settings.xml');
 require_once ROOT_BACK.CONTROLLERS_DIRECTORY.DS.'main.php';
+
+
+/**
+ * Load a config file
+ *
+ * 	Loads a config file (XML) and returns it's values as an array. String-values
+ * 	are returned as strings and integer-values as integers.
+ *
+ *	@param String $path Location of the settings file.
+ *	@return string()|integer()
+ *	@todo Make it work with more complex data types.
+*/
+function load_config($path) {
+	$config = simplexml_load_file($path);
+        
+	foreach ($config->param as $param) {
+		if (!defined($param['name'])) {
+			switch ($param['type']) {
+				case 'string':
+					define($param['name'],$param['value']);
+					break;
+				case 'integer':
+					define($param['name'],(int) $param['value']);
+					break;
+			}
+		}
+	}
+}
 ?>
