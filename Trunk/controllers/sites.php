2379b031b222f3a318f0a52a1e2692ccbbb92616
ssimpo <ssimpo@7b11f61c-ac1f-418c-b1a8-e4117cb018a3>
2011-06-09 19:02:12 +0000

    Moved some of the control code in globals.php to a new controller called sites.php.  The sites controller allows for multiple sites via the one install.  Some more changes needed to make this fully work (next update will move some directories to create separate directories for each site's views and domain files.

---
+++ Trunk/controllers/sites.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ *	Sites controller
+ *
+ *	Will load the site settings for current domain/directory.
+ *
+ *	@author Stephen Simpson <me@simpo.org>
+ *	@version 0.2
+ *	@license http://www.gnu.org/licenses/lgpl.html LGPL
+ */
+
+$sitesConfigPath = get_sites_config_path(ROOT_BACK.CONFIG_DIRECTORY.DS.SITES_FILE);
+load_config($sitesConfigPath);
+
+/**
+ *	Get the path to the current site's config file.
+ *
+ *	@param string $path Path to the sites config file.
+ *	@return string The path to current site's config file.
+ */
+function get_sites_config_path($path) {
+	$config = simplexml_load_file($path);
+	
+	foreach ($config->param as $param) {
+		if (strtolower($param['type']) == 'regx') {
+			if (preg_match($param['value'],DOMAIN)) {
+				return ROOT_BACK.CONFIG_DIRECTORY.DS.$param['src'];
+			} elseif (preg_match($param['value'],ROOT_BACK)) {
+				return ROOT_BACK.CONFIG_DIRECTORY.DS.$param['src'];
+			}
+		}
+	}
+}
+
+/**
+ * Load a config file
+ *
+ * 	Loads a config file (XML) and returns it's values as an array. String-values
+ * 	are returned as strings and integer-values as integers.
+ *
+ *	@param String $path Location of the settings file.
+ *	@return string()|integer()
+ *	@todo Make it work with more complex data types.
+ */
+function load_config($path) {
+	$config = simplexml_load_file($path);
+        
+	foreach ($config->param as $param) {
+		if (!defined($param['name'])) {
+			switch ($param['type']) {
+				case 'string':
+					define($param['name'],$param['value']);
+					break;
+				case 'integer':
+					define($param['name'],(int) $param['value']);
+					break;
+			}
+		}
+	}
+}
+?>
--- Trunk/index.php
+++ Trunk/index.php
@@ -1,4 +1,5 @@
 <?php
 require_once('globals.php');
+require_once ROOT_BACK.CONTROLLERS_DIRECTORY.DS.'sites.php';
 require_once ROOT_BACK.CONTROLLERS_DIRECTORY.DS.'main.php';
 ?>
