20958d3b5f0e9be0873fe03a3faa8bcbb1e2d1b4
Stephen Simpson <me@simpo.org>
2011-06-16 15:11:31 +0000

    Refactored so that each class has a more specific role.

--- analytics/filter.php
+++ analytics/filter.php
@@ -3,8 +3,21 @@ define('DIRECT_ACCESS_CHECK',false);
 require_once('globals.php');
 require_once CONTROLLERS_DIRECTORY.DS.'main.php';
 
-$parser = new LogParser('Domino');
-$dir = SITE_FOLDER.'logs'.DS;
-$parser->parse($dir,'condense');
+$directoryParser = new LogDirectoryParser();
+$directoryParser->add_directory(SITE_FOLDER.'logs'.DS);
+$logParser = new LogParser('Domino','condense');
+
+$filename = $directoryParser->start();
+do {
+    echo "$filename\n";
+    
+    $logParser->file = $filename;
+    do {
+        $line = $logParser->next();
+        print $line."\n";
+    } while ($line !== false);
+    
+    $filename = $directoryParser->next();
+} while ($filename !== false);
 
 ?>
