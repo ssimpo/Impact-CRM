31edc0eb30f859407b1db48c09da012beb2d4b01
ssimpo <ssimpo@7b11f61c-ac1f-418c-b1a8-e4117cb018a3>
2011-06-17 18:37:10 +0000

    Added parsing classes for each type of the FREQ item. Objects are just stubs and do nothing at this stage.

---
+++ Trunk/models/ICalRRuleParser/Base.php
@@ -0,0 +1,16 @@
+<?php
+if (!defined('DIRECT_ACCESS_CHECK')) {
+	die('Direct access is not allowed');
+}
+
+/**
+*	ICalRRuleParser.Base class
+*		
+*	@author Stephen Simpson <me@simpo.org>
+*	@version 0.0.1
+*	@license http://www.gnu.org/licenses/lgpl.html LGPL
+*	@package Calendar
+*/
+class ICalRRuleParser_Base Extends Base {
+   
+}
---
+++ Trunk/models/ICalRRuleParser/Object.php
@@ -0,0 +1,17 @@
+<?php
+if (!defined('DIRECT_ACCESS_CHECK')) {
+	die('Direct access is not allowed');
+}
+
+/**
+ *	The interface for ICalRRuleParser objects
+ *
+ *	@author Stephen Simpson <me@simpo.org>
+ *	@version 0.0.1
+ *	@license http://www.gnu.org/licenses/lgpl.html LGPL
+ *	@package Calendar
+ */
+interface ICalRRuleParser_Object {
+	public function parse($rrule,$start);
+}
+?>
