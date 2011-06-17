27386d08e8c651fd3b3a0125834dea490dcd4d10
ssimpo <ssimpo@7b11f61c-ac1f-418c-b1a8-e4117cb018a3>
2011-06-17 21:51:11 +0000

    Unit Test Stubs for ICalRRuleParser classes.

---
+++ Trunk/util/unit_tests/models/ICalRRuleParser/Secondly.Test.php
@@ -0,0 +1,22 @@
+<?php
+require_once('globals.php');
+
+/**
+ *	Unit Test for the ICalRRuleParser_Secondly class.
+ *
+ *	@author Stephen Simpson <me@simpo.org>
+ *	@version 0.1.1
+ *	@license http://www.gnu.org/licenses/lgpl.html
+ *	@package UnitTests.Impact
+ *	@extends PHPUnit_Framework_TestCase
+ */
+class Test_ICalRRuleParser_Secondly extends ImpactPHPUnit {
+	
+    protected function setUp() {
+        $this->init('ICalRRuleParser_Secondly');
+    }
+	
+	public function test_parse() {
+		//STUB
+	}
+}
