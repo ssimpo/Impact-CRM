daa86f0afbae698a43759fe1cb9ffc8806b5ddbb
ssimpo <ssimpo@7b11f61c-ac1f-418c-b1a8-e4117cb018a3>
2011-06-20 13:56:54 +0000

    Added Unit Tests to Analytics.  Also, added a File class for handling of files an eventually parsing of iCal, XML, logfiles.

--- Trunk/util/UnitTest.bat
+++ Trunk/util/UnitTest.bat
@@ -6,6 +6,7 @@ if "%2" == "all" goto :all
 if "%2" == "models" goto :models
 if "%2" == "plugins" goto :plugins
 if "%2" == "controllers" goto :controllers
+if "%2" == "analytics" goto :analytics
 if "%2" == "" goto :all
 echo Running Unit Tests contained in: %2...
 call phpunit %2 > unittest.log
@@ -29,6 +30,11 @@ goto :end
 :controllers
 echo Running Unit Tests on Impact Models...
 call phpunit unit_tests\controllers\ > unittest.log
+goto :end
+
+:analytics
+echo Running Unit Tests on Analytics Module...
+call phpunit ..\analytics\util\unit_tests\ > unittest.log
 goto :end
 
 :end
