8d6c83eacaf8505991d8cede2a59f1f78cc8925c
ssimpo <ssimpo@7b11f61c-ac1f-418c-b1a8-e4117cb018a3>
2011-06-22 12:44:18 +0000

    Further improvements to autoloading, so there is one location for this functionality.

--- Trunk/controllers/main.php
+++ Trunk/controllers/main.php
@@ -5,44 +5,14 @@ defined('DIRECT_ACCESS_CHECK') or die;
  *	Main controller for the Impact platform.
  *
  *	@author Stephen Simpson <me@simpo.org>
- *	@version 0.1
+ *	@version 0.1.1
  *	@license http://www.gnu.org/licenses/lgpl.html LGPL
  */
-function __autoload($className) {
-	
-	if (!class_exists($className)) {
-		$paths = array(ROOT_BACK.MODELS_DIRECTORY.DS);
-		if (USE_LOCAL_MODELS) {
-			array_unshift($paths,SITE_FOLDER.MODELS_DIRECTORY.DS);
-		}
-		
-		foreach ($paths as $path) {
-			$classFileName = str_replace('_',DS,$className).'.php';
-			
-			if (is_file($path.$classFileName)) {
-				require_once $path.$classFileName;
-				return true;
-			}
-		}
-	}
-	
-	$config = simplexml_load_file(ROOT_BACK.INCLUDES_DIRECTORY.DS.'includes.xml');
-	foreach ($config->param as $param) {
-		if (strtolower($param['name']) == strtolower($className)) {
-			$path = ROOT_BACK.INCLUDES_DIRECTORY.DS.$param['value'];
-			if (is_file($path)) {
-				require_once $path;
-				return true;
-			}
-		}
-	}
-	
-	throw new Exception($className.' Class not found');
-}
 
+require_once ROOT_BACK.INCLUDES_DIRECTORY.DS.'adodb'.DS.'adodb.inc.php';
+require_once('autoload.php');
 
 //Load the main application class and database class
-require_once ROOT_BACK.INCLUDES_DIRECTORY.DS.'adodb'.DS.'adodb.inc.php';
 $application = Application::instance();
 $application->setup();
 $database = Database::instance();
