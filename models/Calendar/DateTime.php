ebf6cc634dd73cbf581f6e5b861f7cc0b9f9fb55
Stephen Simpson <me@simpo.org>
2011-06-24 11:02:33 +0000

    Documentation and Unit Testing for the DateTime class.

--- models/Calendar/DateTime.php
+++ models/Calendar/DateTime.php
@@ -37,27 +37,42 @@ class Calendar_DateTime Extends Base {
 		'','January','February','March','April','May','June',
 		'July','August','September','October','November','December'
 	);
-	private $epoc = 0;
+	public $epoc = 0;
 	
-	public function __construct($year='',$month='',$day='',$hours='',$minutes='',$seconds='') {
+	public function __construct($year=false,$month=false,$day=false,$hours=false,$minutes=false,$seconds=false) {
 		$this->set_date($year,$month,$day,$hours,$minutes,$seconds);
 	}
 	
-	public function set_date($year='',$month='',$day='',$hours='',$minutes='',$seconds='') {
+	/**
+	 *	Set the date/time.
+	 *
+	 *	@public
+	 *	@param int $year The year to set date to (either 2 or 4 digit).
+	 *	@param int|string $month The month to set date to (either 1-2 digit or month name, eg. Jan or January).
+	 *	@param int $day The day to set date to.
+	 *	@param int $hours The hour to set date to.
+	 *	@param int $minutes The minute to set date to.
+	 *	@param int $seconds The second to set date to.
+	 *	@return int The no. of seconds since the Unix epoc for the set date.
+	 */
+	public function set_date($year=false,$month=false,$day=false,$hours=false,$minutes=false,$seconds=false) {
 		$now = getdate();
 		
-		$year = (($year=='')?$now['year']:$year);
-		$month = (($month=='')?$now['mon']:$month);
-		$day = (($day=='')?$now['mday']:$day);
-		$hours = (($hours=='')?$now['hours']:$hours);
-		$minutes = (($minutes=='')?$now['minutes']:$minutes);
-		$seconds = (($seconds=='')?$now['seconds']:$seconds);
+		$year = (($year===false)?$now['year']:$year);
+		$year = $this->_get_four_digit_year($year);
+		$month = (($month===false)?$now['mon']:$month);
+		$month = $this->_get_month_number($month);
+		$day = (($day===false)?$now['mday']:$day);
+		$hours = (($hours===false)?$now['hours']:$hours);
+		$minutes = (($minutes===false)?$now['minutes']:$minutes);
+		$seconds = (($seconds===false)?$now['seconds']:$seconds);
 		$year = $this->_get_four_digit_year($year);
 		$month = $this->_get_month_number($month);
 		
 		$this->epoc = mktime($hours,$minutes,$seconds,$month,$day,$year);
 		return $this->epoc;
 	}
+	
 	
 	public function __get($name) {
 		$date = getdate($this->epoc);
@@ -70,7 +85,7 @@ class Calendar_DateTime Extends Base {
 			case 'hours': return $date['hours'];
 			case 'minutes': return $date['minutes'];
 			case 'seconds': return $date['seconds'];
-			case 'yearday': return $date['yday'];
+			case 'yearday': return ($date['yday']+1);
 			case 'weekday': return $date['weekday'];
 			default:
 				$name = '_get_'.I::uncamelize($name);
@@ -84,22 +99,57 @@ class Calendar_DateTime Extends Base {
 		call_user_func(array($this,$name),$value);
 	}
 	
-	public function ajust($year=0,$month=0,$day=0,$hours=0,$minutes=0,$seconds=0) {
+	/**
+	 *	Adjust a date.
+	 *
+	 *	Given the objects's internally set date, adjust it by the
+	 *	supplied paramaters.
+	 *
+	 *	@public
+	 *	@param int $year The number of years to adjust it by (positive or negative).
+	 *	@param int $month The number of months to adjust it by (positive or negative).
+	 *	@param int $day The number of days to adjust it by (positive or negative).
+	 *	@param int $hours The number of hours to adjust it by (positive or negative).
+	 *	@param int $minutes The number of minutes to adjust it by (positive or negative).
+	 *	@param int $seconds The number of seconds to adjust it by (positive or negative).
+	 *	@return int The no. of seconds since the Unix epoc for the adjusted date.
+	 */
+	public function adjust($year=0,$month=0,$day=0,$hours=0,$minutes=0,$seconds=0) {
 		$this->adjust_year($year);
 		$this->adjust_month($month);
 		$this->adjust_day($day);
 		$this->adjust_hours($hours);
 		$this->adjust_minutes($minutes);
 		$this->adjust_seconds($seconds);
+		if ($day == -25) {
+			print_r(getdate($this->epoc));
+		}
+		return $this->epoc;
 	}
 	
+	/**
+	 *	Adjust date by a supplied number of years.
+	 *
+	 *	@public
+	 *	@param int $amount The number of years to adjust the date by.
+	 *	@return int The no. of seconds since the Unix epoc for the adjusted date.
+	 */
 	public function adjust_year($amount) {
-		$this->_set_part('year',$this->year + $amount);
+		$this->year += $amount;
+		return $this->epoc;
 	}
 	
+	/**
+	 *	Adjust date by a supplied number of months.
+	 *
+	 *	@public
+	 *	@param int $amount The number of months to adjust the date by.
+	 *	@return int The no. of seconds since the Unix epoc for the adjusted date.
+	 */
 	public function adjust_month($amount) {
 		$year = $this->year;
 		$month = $this->month;
+		$factor = $amount;
 		
 		while ($factor != 0) {
 			if ($factor > 0) {
@@ -122,28 +172,77 @@ class Calendar_DateTime Extends Base {
 		
 		$this->year = $year;
 		$this->month = $month;
+		return $this->epoc;
 	}
 	
+	/**
+	 *	Adjust date by a supplied number of weeks.
+	 *
+	 *	@public
+	 *	@param int $amount The number of weeks to adjust the date by.
+	 *	@return int The no. of seconds since the Unix epoc for the adjusted date.
+	 */
 	public function adjust_week($amount) {
 		$this->epoc += ($amount * self::WEEK);
+		return $this->epoc;
 	}
 	
+	/**
+	 *	Adjust date by a supplied number of days.
+	 *
+	 *	@public
+	 *	@param int $amount The number of days to adjust the date by.
+	 *	@return int The no. of seconds since the Unix epoc for the adjusted date.
+	 */
 	public function adjust_day($amount) {
 		$this->epoc += ($amount * self::DAY);
+		return $this->epoc;
 	}
 	
+	/**
+	 *	Adjust date by a supplied number of hours.
+	 *
+	 *	@public
+	 *	@param int $amount The number of hours to adjust the date by.
+	 *	@return int The no. of seconds since the Unix epoc for the adjusted date.
+	 */
 	public function adjust_hours($amount) {
 		$this->epoc += ($amount * self::HOUR);
+		return $this->epoc;
 	}
 	
+	/**
+	 *	Adjust date by a supplied number of minutes.
+	 *
+	 *	@public
+	 *	@param int $amount The number of minutes to adjust the date by.
+	 *	@return int The no. of seconds since the Unix epoc for the adjusted date.
+	 */
 	public function adjust_minutes($amount) {
 		$this->epoc += ($amount * self::MINUTE);
+		return $this->epoc;
 	}
 	
+	/**
+	 *	Adjust date by a supplied number of seconds.
+	 *
+	 *	@public
+	 *	@param int $amount The number of seconds to adjust the date by.
+	 *	@return int The no. of seconds since the Unix epoc for the adjusted date.
+	 */
 	public function adjust_seconds($amount) {
 		$this->epoc += $amount;
+		return $this->epoc;
 	}
 	
+	/**
+	 *	Set part of a date to another value.
+	 *
+	 *	@private
+	 *	@param int $part The amount set the part to.
+	 *	@param string $partName The name of the part to change (hours|minutes|seconds|mon|mday|year).
+	 *	@return The no. of seconds since the Unix epoc for the changed date.
+	 */
 	private function _set_part($part,$partName) {
 		$date = getdate($this->epoc);
 		$date[$partName] = $part;
@@ -155,17 +254,42 @@ class Calendar_DateTime Extends Base {
 		return $this->epoc;
 	}
 	
+	/**
+	 *	Set the year part of a date to another value.
+	 *
+	 *	@private
+	 *	@param int $year The amount set the the year to (either 2-digit or 4 digit year).
+	 *	@return The no. of seconds since the Unix epoc for the changed date.
+	 */
 	private function _set_year($year) {
 		$year = $this->_get_four_digit_year($year);
 		return $this->_set_part($year,'year');
 	}
 	
+	/**
+	 *	Set the month part of a date to another value.
+	 *
+	 *	@note If a negative number is supplied then it is counted backwards from the last month (eg. November is -2).
+	 *
+	 *	@private
+	 *	@param int $month The amount set the the month to (either number, 1-12 or -1--12 or name of month, eg. Jan or January).
+	 *	@return The no. of seconds since the Unix epoc for the changed date.
+	 */
 	private function _set_month($month) {
 		$month = $this->_get_month_number($month);
 		$month = (($month < 0)?(12+$month):$month);
 		return $this->_set_part($month,'mon');
 	}
 	
+	/**
+	 *	Set the day part of a date to another value.
+	 *
+	 *	@note If a negative number is supplied then it is counted backwards from the last day of the month (eg. 29th April would be -2).
+	 *
+	 *	@private
+	 *	@param int $day The amount set the the day to.
+	 *	@return The no. of seconds since the Unix epoc for the changed date.
+	 */
 	private function _set_day($day) {
 		$date = getdate($this->epoc);
 		$month = $this->_get_month_number($date['mon']);
@@ -174,43 +298,124 @@ class Calendar_DateTime Extends Base {
 		return $this->_set_part($day,'mday');
 	}
 	
+	/**
+	 *	Set the hour part of a date to another value.
+	 *
+	 *	@note If a negative number is supplied then it is counted backwards from the end of the day (eg. 23hrs is -2).
+	 *
+	 *	@private
+	 *	@param int $hours The amount set the the hour to.
+	 *	@return The no. of seconds since the Unix epoc for the changed date.
+	 */
 	private function _set_hours($hours) {
 		$hours = (($hours < 0)?(24+$hours):$hours);
 		return $this->_set_part($hours,'hours');
 	}
 	
+	/**
+	 *	Set the minute part of a date to another value.
+	 *
+	 *	@note If a negative number is supplied then it is counted backwards from the end of the hour (eg. 58mins is -2).
+	 *
+	 *	@private
+	 *	@param int $minutes The amount set the the minute to.
+	 *	@return The no. of seconds since the Unix epoc for the changed date.
+	 */
 	private function _set_minutes($minutes) {
 		$minutes = (($minutes < 0)?(60+$minutes):$minutes);
 		return $this->_set_part($minutes,'minutes');
 	}
 	
+	/**
+	 *	Set the second part of a date to another value.
+	 *
+	 *	@note If a negative number is supplied then it is counted backwards from the end of the minute (eg. 58secs is -2).
+	 *
+	 *	@private
+	 *	@param int $seconds The amount set the the second to.
+	 *	@return The no. of seconds since the Unix epoc for the changed date.
+	 */
 	private function _set_seconds($seconds) {
 		$seconds = (($seconds < 0)?(60+$seconds):$seconds);
 		return $this->_set_part($seconds,'seconds');
 	}
 	
+	/**
+	 *	Set the year-day part of a date to another value.
+	 *
+	 *	@note If a negative number is supplied then it is counted backwards from the end of the year (eg. 30th Dec is -2).
+	 *
+	 *	@private
+	 *	@param int $yearDay The amount set the the year-day to.
+	 *	@return The no. of seconds since the Unix epoc for the changed date.
+	 */
 	private function _set_year_day($yearDay) {
-		//STUB
-	}
+		$daysInYear = 365;
+		if ($this->_is_leap_year($this->year)) {
+			$daysInYear = 366;
+		}
+		
+		$this->month = 12;
+		$this->day = 31;
+		if ($yearDay > 0) {
+			$this->year--;
+		} 
 	
+		$this->epoc += (self::DAY * $yearDay);
+		
+		return $this->epoc;
+	}
+
 	private function _get_week() {
 		//STUB
 	}
 	
+	/**
+	 *	Set the week part of a date to another value.
+	 *
+	 *	@note If a negative number is supplied then it is counted backwards from the end of the year (eg. week 52 is normally is -1, for 52 week year).
+	 *
+	 *	@private
+	 *	@param int $week The amount set the the week to.
+	 *	@return The no. of seconds since the Unix epoc for the changed date.
+	 */
 	private function _set_week($week) {
-		//STUB
+		return $this->epoc;
 	}
 	
 	private function _get_week_day_no() {
 		// STUB
 	}
 	
+	/**
+	 *	Set the weekday part of a date to another value.
+	 *
+	 *	@note If a negative number is supplied then it is counted backwards from the end of the week (eg. Friday is -2 if the week starts on Sunday).
+	 *
+	 *	@private
+	 *	@param int|string $weekDay The amount set the the week-day to (no. 1-7 or string in format SU, Sun or Sunday).
+	 *	@return The no. of seconds since the Unix epoc for the changed date.
+	 */
 	private function _set_week_day($weekDay) {
-		//STUB
+		return $this->epoc;
 	}
 	
+	/**
+	 *	Get the day length of a month in a given year.
+	 *
+	 *	@private
+	 *	@param int $year The year to use for the calcultion.
+	 *	@param int|string $month The month to get the number of days in  (digit 1-12 or name, eg. Jan or January).
+	 *	@return int
+	 *	
+	 */
 	private function _get_month_length($year,$month){
-		$month = strtolower(self::$monthName[$month]);
+		if (is_numeric($month)) {
+			$month = strtolower(self::$monthName[$month]);
+		} else {
+			$month = substr(strtolower($month),0,3);
+		}
+		
 		
 		if ($month == 'feb') {
 			if ($this->_is_leap_year($year)) {
@@ -223,6 +428,13 @@ class Calendar_DateTime Extends Base {
 		}
 	}
 	
+	/**
+	 *	Get the month number for a given named month.
+	 *
+	 *	@private
+	 *	@param int|string If a number is given then that is returned, otherwise the month number is found.
+	 *	@return int
+	 */
 	private function _get_month_number($month){
 		if (is_numeric($month)) {
 			return $month;
@@ -259,7 +471,7 @@ class Calendar_DateTime Extends Base {
 	 *	Years 50-99 are set to 1950-1999, whilst years 00-49 are set to
 	 *	2000-2049.  Four digit years are returned unchanged.
 	 *
-	 *	@protected
+	 *	@private
 	 *	@param int $year The two or four digit year.
 	 *	@return int The year adjusted to four digits.
 	 */
--- util/unit_tests/models/Calendar/DateTime.Test.php
+++ util/unit_tests/models/Calendar/DateTime.Test.php
@@ -17,6 +17,108 @@ class Test_Calendar_DateTime extends ImpactPHPUnit {
         $this->init();
 	}
 	
+	public function test_set_date() {
+		$this->assertMethodReturn(
+			mktime(11,0,0,6,24,2011),array(2011,6,24,11,0,0)
+		);
+		$this->assertMethodReturn(
+			mktime(11,0,0,6,24,2010),array(2010,6,24,11,0,0)
+		);
+		$this->assertMethodReturn(
+			mktime(11,0,0,2,29,2012),array(12,'Feb',29,11,0,0)
+		);
+	}
+	
+	public function test_adjust() {
+		$this->instance->set_date(2011,6,24,11,0,0);
+		$this->assertMethodReturn(mktime(11,0,0,6,24,2012),1);
+		$this->assertMethodReturn(
+			mktime(11,0,0,4,24,2012),array(0,-2)
+		);
+		$this->assertMethodReturn(
+			mktime(11,0,0,4,21,2012),array(0,0,-3)
+		);
+		$this->assertMethodReturn(
+			mktime(16,0,0,4,21,2012),array(0,0,0,5)
+		);
+		$this->assertMethodReturn(
+			mktime(15,57,0,4,21,2012),array(0,0,0,0,-3)
+		);
+		$this->assertMethodReturn(
+			mktime(15,57,20,4,21,2012),array(0,0,0,0,0,20)
+		);
+		
+		$this->instance->set_date(2011,6,24,11,0,0);
+		$this->assertMethodReturn(
+			mktime(11,0,0,5,31,2011),array(0,0,-24)
+		);
+	}
+	
+	public function test_adjust_year() {
+		$this->instance->set_date(2011,6,24,11,0,0);
+		$this->assertMethodReturn(mktime(11,0,0,6,24,2012),1);
+		$this->assertMethodReturn(mktime(11,0,0,6,24,2010),-2);
+	}
+	
+	public function test_adjust_month() {
+		$this->instance->set_date(2011,6,24,11,0,0);
+		$this->assertMethodReturn(mktime(11,0,0,7,24,2011),1);
+		$this->assertMethodReturn(mktime(11,0,0,5,24,2011),-2);
+	}
+	
+	public function test_adjust_week() {
+		$this->instance->set_date(2011,6,24,11,0,0);
+		$this->assertMethodReturn(mktime(11,0,0,7,1,2011),1);
+		$this->assertMethodReturn(mktime(11,0,0,6,17,2011),-2);
+	}
+	
+	public function test_adjust_day() {
+		$this->instance->set_date(2011,6,24,11,0,0);
+		$this->assertMethodReturn(mktime(11,0,0,6,25,2011),1);
+		$this->assertMethodReturn(mktime(11,0,0,6,23,2011),-2);
+	}
+	
+	public function test_adjust_hours() {
+		$this->instance->set_date(2011,6,24,11,0,0);
+		$this->assertMethodReturn(mktime(12,0,0,6,24,2011),1);
+		$this->assertMethodReturn(mktime(10,0,0,6,24,2011),-2);
+	}
+	
+	public function test_adjust_minutes() {
+		$this->instance->set_date(2011,6,24,11,0,0);
+		$this->assertMethodReturn(mktime(11,1,0,6,24,2011),1);
+		$this->assertMethodReturn(mktime(10,59,0,6,24,2011),-2);
+	}
+	
+	public function test_adjust_seconds() {
+		$this->instance->set_date(2011,6,24,11,0,0);
+		$this->assertMethodReturn(mktime(11,0,1,6,24,2011),1);
+		$this->assertMethodReturn(mktime(10,59,59,6,24,2011),-2);
+	}
+	
+	public function test_set_part() {
+		$this->instance->set_date(2011,6,24,11,0,0);
+		
+		$this->assertMethodReturn(
+			mktime(11,0,0,6,24,2010),array(2010,'year')
+		);
+		$this->assertMethodReturn(
+			mktime(11,0,0,2,24,2010),array(2,'mon')
+		);
+		$this->assertMethodReturn(
+			mktime(11,0,0,2,23,2010),array(23,'mday')
+		);
+		$this->assertMethodReturn(
+			mktime(12,0,0,2,23,2010),array(12,'hours')
+		);
+		$this->assertMethodReturn(
+			mktime(12,23,0,2,23,2010),array(23,'minutes')
+		);
+		$this->assertMethodReturn(
+			mktime(12,23,23,2,23,2010),array(23,'seconds')
+		);
+	}
+	
 	public function test_set_year() {
 		$this->instance->year = 2010;
 		$this->assertEquals(2010,$this->instance->year);
@@ -73,6 +175,14 @@ class Test_Calendar_DateTime extends ImpactPHPUnit {
 	}
 	
 	public function test_set_year_day() {
+		$this->instance->set_date(2011,6,24,11,0,0);
+		$this->assertMethodReturn(mktime(11,0,0,6,1,2011),152);
+		
+		$this->instance->set_date(2011,6,24,11,0,0);
+		$this->assertMethodReturn(mktime(11,0,0,6,1,2011),-213);
+	}
+	
+	public function test_get_week() {
 		// STUB
 	}
 	
@@ -80,8 +190,42 @@ class Test_Calendar_DateTime extends ImpactPHPUnit {
 		// STUB
 	}
 	
+	public function test_get_week_day() {
+		// STUB
+	}
+	
 	public function test_set_week_day() {
 		// STUB
+	}
+	
+	public function test_get_month_number() {
+		$this->assertMethodReturn(2,'FEB');
+		$this->assertMethodReturn(2,'February');
+		$this->assertMethodReturn(2,2);
+		$this->assertMethodReturn(7,'july');
+	}
+	
+	public function test_get_month_length() {
+		$this->assertMethodReturn(30,array(2012,'april'));
+		$this->assertMethodReturn(30,array(2012,'apr'));
+		$this->assertMethodReturn(30,array(2012,4));
+		$this->assertMethodReturn(29,array(2012,2));
+		$this->assertMethodReturn(28,array(2011,'FEBRUARY'));
+	}
+	
+	public function test_is_leap_year() {
+		$this->assertMethodReturnTrue(2012);
+		$this->assertMethodReturnFalse(2011);
+		$this->assertMethodReturnTrue(2000);
+		$this->assertMethodReturnFalse(1900);
+	}
+	
+	public function test_get_four_digit_year() {
+		$this->assertMethodReturn(2000,0);
+		$this->assertMethodReturn(2012,12);
+		$this->assertMethodReturn(1954,54);
+		$this->assertMethodReturn(1978,78);
+		$this->assertMethodReturn(2003,2003);
 	}
 }
 ?>
