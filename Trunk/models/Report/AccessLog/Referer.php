d9de4666614f3f834189bd0dc156f4ae040cf647
ssimpo <ssimpo@7b11f61c-ac1f-418c-b1a8-e4117cb018a3>
2011-08-05 10:51:18 +0000

    More documentation added and spelling checked.

--- Trunk/models/Report/AccessLog/Referer.php
+++ Trunk/models/Report/AccessLog/Referer.php
@@ -12,14 +12,32 @@ defined('DIRECT_ACCESS_CHECK') or die;
 class Report_AccessLog_Referer extends Report_ReportBase implements Iterator {
 	public $userManager;
 	
+	/**
+	 *	Initialize the object.
+	 *
+	 *	@private
+	 */
 	public function init($userManager) {
 		$this->userManager = $userManager;
 	}
 	
+	/**
+	 *	Close all open session.
+	 *
+	 *	Close all sessions in the connected Report_UserManager object.
+	 *
+	 *	@public
+	 */
 	public function close_all_sessions() {
 		$this->userManager->close_all_sessions();
 	}
 	
+	/**
+	 *	Parse the given data for the referer report.
+	 *
+	 *	@public
+	 *	@param array() $data The data to use.
+	 */
 	public function parse($data) {
 		if ($this->_is_entrance($data)) {
 			$uri = $this->_get_referer_uri($data)->domain;
@@ -36,6 +54,16 @@ class Report_AccessLog_Referer extends Report_ReportBase implements Iterator {
 		}
 	}
 	
+	/**
+	 *	Test whether the user is entering the domain.
+	 *
+	 *	Compare request and referer domain to see if the user has entered
+	 *	the domain with this request or was already active on the site.
+	 *
+	 *	@private
+	 *	@param array() $data The data to use.
+	 *	@return boolean
+	 */
 	public function _is_entrance($data) {
 		$request = str_replace(
 			'www.','',$this->_get_request_uri($data)->domain
@@ -56,6 +84,15 @@ class Report_AccessLog_Referer extends Report_ReportBase implements Iterator {
 		return $this->current;
 	}
 	
+	/**
+	 *	Get the report column headers.
+	 *
+	 *	Get the report column headers, which is extremely useful when printing
+	 *	CSV to a file.
+	 *
+	 *	@public
+	 *	@return array()
+	 */
 	public function headers() {
 		return array_keys($this->_create_report_entry());
 	}
@@ -91,6 +128,16 @@ class Report_AccessLog_Referer extends Report_ReportBase implements Iterator {
 		return $url;
 	}
 	
+	/**
+	 *	Get the referer URI.
+	 *
+	 *	From the supplied data array, return a Filesystem_Path object, which
+	 *	represents the referer
+	 *
+	 *	@private
+	 *	@param array() $data The data to parse for a referer URI.
+	 *	@return Filesystem_Path
+	 */
 	private function _get_referer_uri($data) {
 		$url = $data['referer'];
 		$url = new Filesystem_Path(strtolower($url));
